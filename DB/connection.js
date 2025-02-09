a9d82a99f1a7742c30bff03ab02d4758e0bacbd7
eidabdallah <eidabdallah971@gmail.com>
2025-02-09 19:07:43 +0200

    update connection

--- DB/connection.js
+++ DB/connection.js
@@ -1,11 +1,9 @@
 import { Sequelize } from 'sequelize';
-
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.HOST,
     port: 3306,
     dialect: 'mysql'
 });
-
 export const connectDB = async () => {
     try {
         await sequelize.sync();
