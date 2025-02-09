84ba731b7b5454cb42156638130057bd1a068c68
eidabdallah <eidabdallah971@gmail.com>
2025-02-09 18:53:50 +0200

    update connection page

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('saraha', 'root', '', {
-    host: 'localhost',
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.HOST,
+    port: 3306,
     dialect: 'mysql'
 });
 
