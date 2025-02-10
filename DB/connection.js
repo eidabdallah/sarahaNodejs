13465e555aee1cef8cc5d07cd7607336b0127394
eidabdallah <eidabdallah971@gmail.com>
2025-02-10 20:47:25 +0200

    update connection

--- DB/connection.js
+++ DB/connection.js
@@ -1,4 +1,6 @@
 import { Sequelize } from 'sequelize';
+import 'dotenv/config';
+
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.HOST,
     port: 3306,
@@ -9,6 +11,6 @@ export const connectDB = async () => {
         await sequelize.sync();
         console.log('Connected to the database successfully......');
     } catch (error) {
-        console.error('Error connecting to the database :', error.message);
+        console.error('Error connecting to the database :', error || error.message);
     }
 }
