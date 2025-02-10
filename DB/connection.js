4d2a2ec5680cdcbe083e021755445cfcd8dd41c8
eidabdallah <eidabdallah971@gmail.com>
2025-02-10 20:53:30 +0200

    update connection

--- DB/connection.js
+++ DB/connection.js
@@ -1,5 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { Sequelize } from 'sequelize';
-import 'dotenv/config';
 
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.HOST,
