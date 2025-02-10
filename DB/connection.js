695a79b4e5b60727a0becd369c2c5111ef496663
eidabdallah <eidabdallah971@gmail.com>
2025-02-10 20:59:31 +0200

    update connection

--- DB/connection.js
+++ DB/connection.js
@@ -1,9 +1,6 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import { Sequelize } from 'sequelize';
-
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.HOST,
+export const sequelize = new Sequelize("freedb_saraha", "freedb_EidAbdallah@", "WF5dV*rwPEbSsJC", {
+    host: "sql.freedb.tech",
     port: 3306,
     dialect: 'mysql'
 });
